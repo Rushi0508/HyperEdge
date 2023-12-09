a4e042b1d6cd7c898131b6777033227fc4e41b0e
Rushi_0508 <rushigandhi14@gmail.com>
2023-12-09 22:44:01 +0530

    feat: hero section

--- src/app/(assets)/index.ts
+++ src/app/(assets)/index.ts
@@ -9,6 +9,7 @@ import arrowUp from "./arrow-up.svg";
 import discount from "./Discount.svg";
 import instagram from "./instagram.svg";
 import twitter from "./twitter.svg";
+import hero from './hero.png'
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
@@ -24,6 +25,7 @@ export {
   arrowUp,
   discount,
   instagram,
+  hero,
   twitter,
   people01,
   people02,
--- src/app/(components)/Hero.tsx
+++ src/app/(components)/Hero.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import styles from "../style";
+import { discount, hero } from "../(assets)";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} items-center`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month</span> Account
+          </p>
+        </div>
 
-export default Hero
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            Empowering<br className="sm:block hidden" />{" "}
+            <span className="text-gradient whitespace-nowrap">Next Generation</span>{" "}
+          </h1>
+        </div>
+
+        <h1 className="font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full whitespace-nowrap">
+          of Influence
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Discover endless possibilities on HyperEdge. Strengthen your brand or amplify your voice in the vibrant world of influencer marketing. Get started now.
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative border-1 border-red-500`}>
+        <Image src={hero} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
