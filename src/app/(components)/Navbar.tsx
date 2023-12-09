e65b149e92559d69a0ade7b65221ea85bdf5fa5a
Rushi_0508 <rushigandhi14@gmail.com>
2023-12-09 18:39:53 +0530

    project setup

---
+++ src/app/(components)/Navbar.tsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+import { close, logo, menu } from '../(assets)'
+
+const Navbar = () => {
+  return (
+    <nav>
+
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/app/(components)/index.ts
@@ -0,0 +1,11 @@
+import GetStarted from "./GetStarted";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Clients from "./Clients";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import CTA from "./CTA";
+
+export {
+    Navbar, GetStarted, Hero, CTA, Stats, Footer, Clients
+}
