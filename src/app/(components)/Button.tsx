ed97d6ead8e7083da9beb7580690028c9e02df59
Rushi_0508 <rushigandhi14@gmail.com>
2023-12-10 14:46:57 +0530

    feat: getting started page

--- src/app/(components)/Button.tsx
+++ src/app/(components)/Button.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import React from "react";
 
 const Button = ({ styles }:any) => (
-  <button type="button" className={`text-[15px] py-2 px-4 xs:py-2 xs:px-6 font-medium xs:text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    Get Started
-  </button>
+  <Link href='/getting-started' type="button" className={`text-[15px] py-2 px-4 xs:py-2 xs:px-6 font-medium xs:text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    Get Started for Free
+  </Link>
 );
 
 export default Button;
