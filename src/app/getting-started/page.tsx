247d1b477b9cc214c538fe84c0cef210d8c296a1
Rushi_0508 <rushigandhi14@gmail.com>
2023-12-10 14:56:59 +0530

    fix: removing usestate from page.tes

--- src/app/getting-started/page.tsx
+++ src/app/getting-started/page.tsx
@@ -1,8 +1,6 @@
 "use client"
-import { useState } from "react"
 import GettingStarted from "./GettingStarted"
 const page = () => {
-    const [role, setRole] = useState("-1")
   return (
     <GettingStarted/>
   )
