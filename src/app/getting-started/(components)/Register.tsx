0e095db0a2ccf99b18ec5d3bb1b5c1c9bff7df7a
Rushi_0508 <rushigandhi14@gmail.com>
2023-12-22 15:39:24 +0530

    fix: added min. password length

--- src/app/getting-started/(components)/Register.tsx
+++ src/app/getting-started/(components)/Register.tsx
@@ -42,6 +42,7 @@ const Register = ({setConfirm}:any) => {
     const handleSubmit = async ()=>{
         if(details.role=="-1") return toast.error("Select your Role")
         else if(isAnyFieldEmpty()) return toast.error("Please enter all the details")
+        else if(details.password.length<6) return toast.error("Password must have atleast 6 characters")
         var validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
         if(!details.email.match(validEmail)){
             return toast.error("Enter a valid email address")
